40548cfa1fa18af6cc5a907fe5f64573fd33b4b6
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-24 18:23:54 +0700

    Update chat.ts

--- api/chat.ts
+++ api/chat.ts
@@ -72,149 +72,188 @@ export default async function handler(req, res) {
     "mengajukan bantuan"
   ];
 
-  // =========================================================
-  // 🔥 CUSTOM RESPONSES
-  // =========================================================
-  const customResponses = [
+// =========================================================
+// 🔥 CUSTOM RESPONSES
+// =========================================================
+const customResponses = [
 
-    // 📄 SURAT DOMISILI
-    {
-      keywords: [
-        "buat surat domisili",
-        "mau buat surat domisili",
-        "surat domisili",
-        "domisili",
-        "skdu",
-        "buat skdu"
-      ],
-      reply: `
+  // 📄 SURAT DOMISILI
+  {
+    keywords: [
+      "buat surat domisili",
+      "mau buat surat domisili",
+      "surat domisili",
+      "domisili",
+      "skdu",
+      "buat skdu"
+    ],
+
+    reply: `
 Oke, silakan isi datamu jika ingin membuat Surat Domisili di link berikut ya :<br><br>
 
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPTARAcNT7gh4F8I4mFy2S7BL6hkQiFQNx5KhhNTSTDTaM9A/viewform?usp=header" target="_blank" rel="noopener noreferrer">
 📄 Form Surat Domisili
 </a>
 `
-    },
+  },
 
-    // 📄 SURAT KETERANGAN USAHA
-    {
-      keywords: [
-        "buat surat keterangan usaha",
-        "mau buat surat keterangan usaha",
-        "surat keterangan usaha",
-        "sku",
-        "buat sku",
-        "mau buat sku",
-        "surat usaha"
-      ],
-      reply: `
+  // 📄 SURAT KETERANGAN USAHA
+  {
+    keywords: [
+      "buat surat keterangan usaha",
+      "buatkan surat keterangan usaha",
+      "mau buat surat keterangan usaha",
+      "surat keterangan usaha",
+      "sku",
+      "buat sku",
+      "mau buat sku",
+      "surat usaha"
+    ],
+
+    reply: `
 Oke, silakan isi datamu jika ingin membuat Surat Keterangan Usaha di link berikut ya :<br><br>
 
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSfwKiGjCUQaAbebp0khcr0eKGYHKwNdnmKfYmMaq_6NLE6yfw/viewform?usp=header" target="_blank" rel="noopener noreferrer">
 📄 Form Surat Keterangan Usaha
 </a>
 `
-    },
+  },
 
-    // 📄 SURAT KETERANGAN TIDAK MAMPU
-    {
-      keywords: [
-        "buat surat keterangan tidak mampu",
-        "mau buat surat keterangan tidak mampu",
-        "surat keterangan tidak mampu",
-        "sktm",
-        "buat sktm",
-        "mau buat sktm",
-        "surat tidak mampu",
-        "surat keterangan miskin"
-      ],
-      reply: `
-Oke, silakan isi datamu jika ingin membuat Surat Keterangan Tidak Mampu di link berikut ya :<br><br>
-
-<a href="https://docs.google.com/forms/d/e/1FAIpQLSfZwvH39BgqzMZAU8q9qodU1SXsktu8xgVUvA4LLovsb5Wosg/viewform?usp=header" target="_blank" rel="noopener noreferrer">
-📄 Form Surat Keterangan Tidak Mampu
+  // 📄 SURAT DOMISILI
+  {
+    keywords: [
+      "buat surat domisili",
+      "mau buat surat domisili",
+      "surat domisili",
+      "domisili",
+      "skdu",
+      "buat skdu"
+    ],
+    reply: `
+Oke, silakan isi datamu jika ingin membuat Surat Domisili di link berikut ya :<br><br>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfPTARAcNT7gh4F8I4mFy2S7BL6hkQiFQNx5KhhNTSTDTaM9A/viewform?usp=header" target="_blank" rel="noopener noreferrer">
+📄 Form Surat Domisili
 </a>
 `
-    },
+  },
 
-    // 📄 SURAT KETERANGAN PINDAH
-    {
-      keywords: [
-        "buat surat keterangan pindah",
-        "mau buat surat keterangan pindah",
-        "surat keterangan pindah",
-        "skpk",
-        "buat skpk",
-        "mau buat skpk",
-        "surat pindah"
-      ],
-      reply: `
+  // 📄 SURAT KETERANGAN USAHA
+  {
+    keywords: [
+      "buat surat keterangan usaha",
+      "mau buat surat keterangan usaha",
+      "surat keterangan usaha",
+      "sku",
+      "buat sku",
+      "mau buat sku",
+      "surat usaha"
+    ],
+    reply: `
+Oke, silakan isi datamu jika ingin membuat Surat Keterangan Usaha di link berikut ya :<br><br>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfwKiGjCUQaAbebp0khcr0eKGYHKwNdnmKfYmMaq_6NLE6yfw/viewform?usp=header" target="_blank" rel="noopener noreferrer">
+📄 Form Surat Keterangan Usaha
+</a>
+`
+  },
+
+  // 📄 SKTM SEKOLAH
+  {
+    keywords: [
+      "sktm sekolah",
+      "surat keterangan tidak mampu sekolah",
+      "sktm pendidikan",
+      "sktm kuliah"
+    ],
+    reply: `
+Oke, silakan isi datamu jika ingin membuat Surat Keterangan Tidak Mampu di link berikut ya :<br><br>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfZwvH39BgqzMZAU8q9qodU1SXsktu8xgVUvA4LLovsb5Wosg/viewform?usp=header" target="_blank" rel="noopener noreferrer">
+📄 Form SKTM Sekolah
+</a>
+`
+  },
+
+  // 📄 SKTM UMUM
+  {
+    keywords: [
+      "sktm umum",
+      "surat keterangan tidak mampu umum"
+    ],
+    reply: `Belum di koding`
+  },
+
+  // 📄 SURAT KETERANGAN PINDAH
+  {
+    keywords: [
+      "buat surat keterangan pindah",
+      "mau buat surat keterangan pindah",
+      "surat keterangan pindah",
+      "skpk",
+      "buat skpk",
+      "mau buat skpk",
+      "surat pindah"
+    ],
+    reply: `
 Oke, silakan isi datamu jika ingin membuat Surat Keterangan Pindah di link berikut ya :<br><br>
-
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdemKtISdr-g7JGgcuVGUySyypmA2Njjmg-lT-2eRzMaikDVA/viewform?usp=header" target="_blank" rel="noopener noreferrer">
 📄 Form Surat Keterangan Pindah
 </a>
 `
-    },
+  },
 
-    // 📄 SURAT KETERANGAN PINDAH DATANG
-    {
-      keywords: [
-        "buat surat keterangan pindah datang",
-        "mau buat surat keterangan pindah datang",
-        "surat keterangan pindah datang",
-        "skpd",
-        "buat skpd",
-        "mau buat skpd",
-        "surat pindah datang"
-      ],
-      reply: `
+  // 📄 SURAT KETERANGAN PINDAH DATANG
+  {
+    keywords: [
+      "buat surat keterangan pindah datang",
+      "mau buat surat keterangan pindah datang",
+      "surat keterangan pindah datang",
+      "skpd",
+      "buat skpd",
+      "mau buat skpd",
+      "surat pindah datang"
+    ],
+    reply: `
 Oke, silakan isi datamu jika ingin membuat Surat Keterangan Pindah Datang di link berikut ya :<br><br>
-
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAjswrbwP-6XZyrXVxJEMZiB7lPXxjGd4ja71M8AbtKfQVHQ/viewform?usp=header" target="_blank" rel="noopener noreferrer">
 📄 Form Surat Keterangan Pindah Datang
 </a>
 `
-    },
+  },
 
-    // 📄 SURAT KETERANGAN BELUM NIKAH
-    {
-      keywords: [
-        "buat surat keterangan belum nikah",
-        "mau buat surat keterangan belum nikah",
-        "surat keterangan belum nikah",
-        "buat surat keterangan belum menikah",
-        "mau buat surat keterangan belum menikah",
-        "surat keterangan belum menikah",
-        "skbn",
-        "buat skbn",
-        "mau buat skbn",
-        "surat belum nikah"
-      ],
-      reply: `
+  // 📄 SURAT KETERANGAN BELUM NIKAH
+  {
+    keywords: [
+      "buat surat keterangan belum nikah",
+      "mau buat surat keterangan belum nikah",
+      "surat keterangan belum nikah",
+      "buat surat keterangan belum menikah",
+      "mau buat surat keterangan belum menikah",
+      "surat keterangan belum menikah",
+      "skbn",
+      "buat skbn",
+      "mau buat skbn",
+      "surat belum nikah"
+    ],
+    reply: `
 Oke, silakan isi datamu jika ingin membuat Surat Keterangan Belum Nikah di link berikut ya :<br><br>
-
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSfbTBRQr_Pd-o5UjcKcSH3cyeWDFQTKaaWJjQXGuS8oIBLWeg/viewform?usp=header" target="_blank" rel="noopener noreferrer">
 📄 Form Surat Keterangan Belum Nikah
 </a>
 `
-    },
+  },
 
-    // 👋 SAPAAN (taruh paling bawah)
-    {
-      keywords: [
-        "hai selamat malam",
-        "hai selamat pagi",
-        "hai selamat siang",
-        "hai selamat sore",
-        "hai",
-        "halo",
-        "aira"
-      ],
-      reply: `Halo 👋 Ada yang bisa saya bantu?`
-    }
-
-  ];
+  // 👋 SAPAAN
+  {
+    keywords: [
+      "hai selamat malam",
+      "hai selamat pagi",
+      "hai selamat siang",
+      "hai selamat sore",
+      "hai",
+      "halo"
+    ],
+    reply: `Halo 👋 Ada yang bisa saya bantu?`
+  }
+];
 
   // =========================================================
   // 🔎 JAWABAN KHUSUS
@@ -362,3 +401,4 @@ Silakan isi form bantuan berikut ya:<br><br>
   }
 
 }
+
