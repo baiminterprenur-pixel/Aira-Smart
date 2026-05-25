72203bbbea1ad872ee4453b93ffb8e603410c8b1
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-25 21:17:30 +0700

    Create suratKeteranganTanah.ts

---
+++ modules/surat/suratKeteranganTanah.ts
@@ -0,0 +1,40 @@
+// ======================================================
+// 🌾 INTENT SURAT KETERANGAN TANAH - AIRA CHAT
+// File: modules/intents/suratKeteranganTanah.ts
+// ======================================================
+
+export const suratKeteranganTanah = {
+
+  keywords: [
+    "surat tanah",
+    "keterangan tanah",
+    "surat keterangan tanah",
+    "surat sporadik",
+    "surat lahan",
+    "surat kebun",
+    "surat kepemilikan tanah",
+    "surat tanah desa",
+    "tanah warisan",
+    "tanah kebun",
+    "surat penguasaan tanah",
+    "surat alas hak",
+    "administrasi tanah",
+    "buat surat tanah",
+    "ngurus surat tanah",
+    "data tanah",
+    "kepemilikan lahan",
+    "surat ukur tanah",
+    "tanah warga"
+  ],
+
+  reply: `
+Baik 😊
+
+Untuk pengurusan Surat Keterangan Tanah, silakan isi formulir berikut:
+
+https://forms.gle/LINK_GOOGLE_FORM_TANAH
+
+Pastikan data kepemilikan dan lokasi tanah diisi dengan lengkap dan benar agar proses administrasi dapat dibantu oleh Aira.
+`
+
+};
