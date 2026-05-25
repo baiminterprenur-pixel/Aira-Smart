4d3ff8a063e2ddb9173a5f699fad7b93fcce2472
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-25 21:49:45 +0700

    Create index.ts

---
+++ modules/intents/index.ts
@@ -0,0 +1,33 @@
+import { suratKematian } from "./suratKematian";
+import { suratSKCK } from "./suratSKCK";
+import { suratKelahiran } from "./suratKelahiran";
+import { suratAhliWaris } from "./suratAhliWaris";
+import { suratKehilangan } from "./suratKehilangan";
+import { suratPengantarKTP } from "./suratPengantarKTP";
+import { suratPengantarKK } from "./suratPengantarKK";
+import { suratIzinKeramaian } from "./suratIzinKeramaian";
+import { suratJandaDuda } from "./suratJandaDuda";
+import { suratPenghasilanOrangTua } from "./suratPenghasilanOrangTua";
+import { suratKeteranganTanah } from "./suratKeteranganTanah";
+import { suratBedaNama } from "./suratBedaNama";
+import { suratBepergian } from "./suratBepergian";
+import { suratBPJSKIS } from "./suratBPJSKIS";
+import { suratAktifSekolahKuliah } from "./suratAktifSekolahKuliah";
+
+export const allIntents = [
+  suratKematian,
+  suratSKCK,
+  suratKelahiran,
+  suratAhliWaris,
+  suratKehilangan,
+  suratPengantarKTP,
+  suratPengantarKK,
+  suratIzinKeramaian,
+  suratJandaDuda,
+  suratPenghasilanOrangTua,
+  suratKeteranganTanah,
+  suratBedaNama,
+  suratBepergian,
+  suratBPJSKIS,
+  suratAktifSekolahKuliah
+];
