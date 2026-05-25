7af24ffcc2006a37707e98a022da50dfecd695a5
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-25 22:18:47 +0700

    Create index.ts

---
+++ modules/surat/index.ts
@@ -0,0 +1,35 @@
+import { suratKematian } from "./suratKematian";
+import { suratSKCK } from "./suratSKCK";
+import { suratKelahiran } from "./suratKelahiran";
+import { suratAhliWaris } from "./suratAhliWaris";
+import { suratKehilangan } from "./suratKehilangan";
+import { suratPengantarKTP } from "./suratPengantarKTP";
+import { suratPengantarKK } from "./suratPengantarKK";
+import { suratIzinKeramaian } from "./suratIzinKeramaian";
+import { suratJandaDuda } from "./suratJandaDuda";
+import { suratPenghasilanOrangTua } from "./suratPenghasilanOrangTua";
+import { suratKeteranganTanah } from "./suratKeteranganTanah";
+import { suratBedaNama } from "./suratBedaNama";
+import { suratBepergian } from "./suratBepergian";
+import { suratBPJSKIS } from "./suratBPJSKIS";
+import { suratAktifSekolahKuliah } from "./suratAktifSekolahKuliah";
+import { suratNikah } from "./suratNikah";
+
+export const allSurat = [
+  suratKematian,
+  suratSKCK,
+  suratKelahiran,
+  suratAhliWaris,
+  suratKehilangan,
+  suratPengantarKTP,
+  suratPengantarKK,
+  suratIzinKeramaian,
+  suratJandaDuda,
+  suratPenghasilanOrangTua,
+  suratKeteranganTanah,
+  suratBedaNama,
+  suratBepergian,
+  suratBPJSKIS,
+  suratAktifSekolahKuliah,
+  suratNikah
+];
