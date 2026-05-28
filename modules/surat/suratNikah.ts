ef3acddec2aafd4e20d887b0268543178313ae49
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-28 07:46:25 +0700

    Update suratNikah.ts

--- modules/surat/suratNikah.ts
+++ modules/surat/suratNikah.ts
@@ -1,15 +1,17 @@
 export default {
   keywords: [
-    keywords: [
-  "surat nikah",
-  "buat surat nikah",
-  "mau buat surat nikah",
-  "surat pengantar nikah",
-  "nikah",
-  "surat keterangan nikah"
-]
+    "surat nikah",
+    "buat surat nikah",
+    "mau buat surat nikah",
+    "surat pengantar nikah",
+    "nikah",
+    "surat keterangan nikah"
+  ],
 
   reply: `
-Silakan isi form surat nikah di bawah ini:
+Silakan isi form Surat Nikah di link berikut ya :<br><br>
+<a href="https://docs.google.com/forms/d/e/LINK_FORM_SURAT_NIKAH/viewform?usp=header" target="_blank" rel="noopener noreferrer">
+📄 Form Surat Nikah
+</a>
 `
 };
