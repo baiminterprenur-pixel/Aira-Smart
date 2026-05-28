8a0526f4d67a6a87dbbe3450a1b87d8974e11c66
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-28 07:50:12 +0700

    Update index.ts

--- modules/surat/index.ts
+++ modules/surat/index.ts
@@ -1,3 +1,22 @@
+// Import semua intent surat
+import suratKematian from "./suratKematian";
+import suratSKCK from "./suratSKCK";
+import suratKelahiran from "./suratKelahiran";
+import suratAhliWaris from "./suratAhliWaris";
+import suratKehilangan from "./suratKehilangan";
+import suratPengantarKTP from "./suratPengantarKTP";
+import suratPengantarKK from "./suratPengantarKK";
+import suratIzinKeramaian from "./suratIzinKeramaian";
+import suratJandaDuda from "./suratJandaDuda";
+import suratPenghasilanOrangTua from "./suratPenghasilanOrangTua";
+import suratKeteranganTanah from "./suratKeteranganTanah";
+import suratBedaNama from "./suratBedaNama";
+import suratBepergian from "./suratBepergian";
+import suratBPJSKIS from "./suratBPJSKIS";
+import suratAktifSekolahKuliah from "./suratAktifSekolahKuliah";
+import suratNikah from "./suratNikah";
+
+// Export default semua intent surat
 export default {
   suratKematian,
   suratSKCK,
